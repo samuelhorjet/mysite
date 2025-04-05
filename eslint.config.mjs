cd4278e2d3838c4309eb882ffbefbc30849e278d
samuelhorjet <samuelhorjet001@gmail.com>
2025-04-05 11:50:44 +0100

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Create the base ESLint config
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',  // Disable unused-vars rule
+      // You can add more rules here if needed
+    },
+  },
 ];
 
 export default eslintConfig;
